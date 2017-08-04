b3fe1855d64a4805a353ab8cfb64f3f83af05b09
Konstantin Vasilev <kon.vasilevclass@gmail.com>
2017-08-04 05:42:29 -0700

    JUnitTest properly renamed

--- src/test/java/core/JUnitTest.java
+++ src/test/java/core/JUnitTest.java
@@ -15,31 +15,31 @@ public class JUnitTest {
 
     @Before
     public static void before() {
-        System.out.println("Before Test");
+        System.out.println("\tBefore Test");
     }
 
     @After
-    public static void after("After Test") {
+    public static void after("\tAfter Test") {
         System.out.println();
     }
 
     @Test
     public void test_01() {
-        System.out.println("Test # 01");
+        System.out.println("\t\tTest # 01");
     }
 
     @Test
     public void test_02() {
-        System.out.println("Test # 02");
+        System.out.println("\t\tTest # 02");
     }
 
     @Test
     public void test_03() {
-        System.out.println("Test # 03");
+        System.out.println("\t\tTest # 03");
     }
 
     @Test
     public void test_04() {
-        System.out.println("Test # 04");
+        System.out.println("\t\tTest # 04");
     }
 }
